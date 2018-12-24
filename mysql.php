8433e420e07e34142e3ff7e2b9f83078a4b94477
nakkaa <tnakkaa@gmail.com>
2018-12-24 20:51:49 +0900

    READMEを追加

--- mysql.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-Flight::register('db', 'mysqli', array('localhost','my_user','my_pass','my_dbname'));
-
-$db = Flight::db();
-
-$x = $db->query("SELECT * FROM `test_table` LIMIT 1")->fetch_assoc();
-
-print_r($x);
